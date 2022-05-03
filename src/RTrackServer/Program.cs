1cd730d1765df1f36c14806610c1ad088e3792d7
Ruxo Zheng <rz@just.sent.as>
2022-05-04 05:49:29 +0700

    Refactor: Use minimal-style setup

--- src/RTrackServer/Program.cs
+++ src/RTrackServer/Program.cs
@@ -1,20 +1,72 @@
-using Microsoft.AspNetCore.Hosting;
+using System;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RTrackServer.Services;
 
-namespace RTrackServer
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<ITrackerService, TrackerService>();
+builder.Services.AddHostedService(sp => (TrackerService)sp.GetRequiredService<ITrackerService>());
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+var credentials = builder.Configuration.GetRequiredSection("TiraxAuthenticator").Get<OAuthCredentials>();
+
+builder.Services.AddAuthentication(opt => {
+            opt.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            opt.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            opt.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+        })
+       .AddCookie()
+       .AddOpenIdConnect("oidc",
+                         opts => {
+                             opts.Authority = "https://auth.tirax.tech/";
+                             opts.ClientSecret = credentials.ClientSecret;
+                             opts.ClientId = credentials.ClientId;
+                             opts.UsePkce = true;
+                             opts.ResponseType = "code";
+                             opts.SaveTokens = true;
+                             opts.GetClaimsFromUserInfoEndpoint = true;
+                             opts.Scope.Add("openid");
+                             opts.Scope.Add("profile");
+                             opts.TokenValidationParameters = new(){ NameClaimType = "name" };
+                         });
+
+var app = builder.Build();
+
+var basePath = builder.Configuration["BasePath"] ?? "/";
+Console.WriteLine("BasePath = {0}", basePath);
+app.UsePathBase(basePath);
+if (builder.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else {
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseAuthentication();
+app.UseAuthentication();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    endpoints.MapBlazorHub();
+    endpoints.MapFallbackToPage("/_Host");
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+app.Run();
+
+sealed class OAuthCredentials
+{
+    public string ClientId { get; set; } = string.Empty;
+    public string ClientSecret { get; set; } = string.Empty;
 }
